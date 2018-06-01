38848583c9f66ea31edea6c4ca67482130ddb42c
Yadiel <ycabr035@fiu.edu>
2018-06-01 19:00:17 -0400

    added 2nd reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,11 +1,12 @@
 import C from '../constants'
 
-export const goal = (state=10, action) => {
+export const goal = (state=10, action) =>
+	(action.type === C.SET_GOAL) ?
+		 parseInt(action.payload) :
+		 state
 
-   if (action.type === C.SET_GOAL) {
-   	   return parseInt(action.payload)
-   } else {
-       return state
-   }
 
-}
+export const skiDay = (state=null, action) =>
+  (action.type === C.ADD_DAY) ?
+  	action.payload :
+  	state
