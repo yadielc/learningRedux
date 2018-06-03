97fbf2db15fdc122f49bb76456db9a118648d110
Yadiel <ycabr035@fiu.edu>
2018-06-03 19:43:02 -0400

    added showErrors container

--- src/components/containers/ShowErrors.js
+++ src/components/containers/ShowErrors.js
@@ -1,5 +1,21 @@
 import ShowErrors from '../ui/ShowErrors'
+import { clearError } from '../../actions'
+import { connect } from 'react-redux'
 
-export default () =>
-	<ShowErrors errors={['sample error']}
-						  onClearError={index => console.log('todo: clear error at', index)} />
+const mapStateToProps = state => {
+	return {
+		errors: state.errors
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		onClearError(index) {
+			dispatch(
+				clearError(index)
+			)
+		}
+	}
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(ShowErrors)
