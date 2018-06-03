fcb6c7e157311526457d9b2d66fcc6dd8bf70092
Yadiel <ycabr035@fiu.edu>
2018-06-03 17:25:16 -0400

    added actions

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,5 @@
 import C from './constants'
+import fetch from 'isomorphic-fetch'
 
 export function addDay(resort, date, powder=false, backcountry=false) {
 
@@ -47,22 +48,32 @@ export const clearSuggestions = () =>
 		type: C.CLEAR_SUGGESTIONS
 	})
 
-export const randomGoals = () = (dispatch, getState) => {
+export const suggestResortNames = value => dispatch => {
 
-	if (!getState.resortNames.fetching) {
+	dispatch({
+		type: C.FETCH_RESORT_NAMES
+	})
 
-		dispatch({
-			type: C.FETCH_RESORT_NAMES
+	fetch('http://localhost:3333/resorts/' + value)
+		.then(response => response.json())
+		.then(suggestions => {
+
+			dispatch({
+				type: C.CHANGE_SUGGESTIONS,
+				payload: suggestions
+			})
+
 		})
+		.catch(error => {
 
-		setTimeout(() => {
+			dispatch(
+				addError(error.message)
+			)
 
 			dispatch({
 				type: C.CANCEL_FETCHING
 			})
 
-		}, 1500)
-
-	}
+		})
 
 }
